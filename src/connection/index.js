facf9a5c65b7932d111c35b20251d7693fec6d51
Putra Nugroho <putraa.nugroho@gmail.com>
2022-03-24 10:21:12 +0700

    Update index.js

--- src/connection/index.js
+++ src/connection/index.js
@@ -2,16 +2,17 @@ const mysql = require('mysql')
 
 const conn = mysql.createConnection(
     {
-        user:'root',
-        password:'',
-        host:'localhost',
-        database:'project_hris_db',
-        port:3306
+        // user:'root',
+        // password:'',
+        // host:'localhost',
+        // database:'project_hris_db',
+        // port:3306
         
-        // host:'us-cdbr-east-03.cleardb.com',
-        // user:'bb9c2f6d9ed352',
-        // password:'a768e645',
-        // database:'heroku_94e0f5b406e00d0'
+        host:'us-mm-auto-dca-05-b.cleardb.net',
+        user:'bb9c2f6d9ed352',
+        password:'a768e645',
+        database:'heroku_94e0f5b406e00d0',
+        port:3306
     }
 )
 
