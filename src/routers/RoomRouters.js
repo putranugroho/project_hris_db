10a9f9a954b49a0471e425f020f440d7a2dc10d5
Putra Nugroho <putraa.nugroho@gmail.com>
2022-03-25 15:00:33 +0700

    v0.4
    Fix Booking Reservation and SQL Time Slot

--- src/routers/RoomRouters.js
+++ src/routers/RoomRouters.js
@@ -74,13 +74,15 @@ router.get('/rooms', (req,res)=>{
 })
 
 router.post('/booking', (req,res)=>{
-    const sql_reservation = `INSERT INTO tb_reservation(reservation_id, pic, jabatan, agenda, time, addon, user_booking, stsrec) VALUES (?,?,?,?,?,?,?,0)`
+    const sql_reservation = `INSERT INTO tb_reservation(reservation_id, pic, jabatan, agenda, time, addon, participant, user_booking, stsrec) VALUES (?,?,?,?,?,?,?,?,0)`
     const sql_participant = `INSERT INTO tb_participant(nik, reservation_id, nama, hp, email, reminder, status) VALUES (?,?,?,?,?,?,0)`
     const sql_time_slot = `INSERT INTO tb_time_slot_detail(room_id, time_slot_id, reservation_id, date) VALUES (?,?,?,?)`
-    const count = `SELECT COUNT(*) AS 'count' FROM tb_reservation`
+    const count = `SELECT count_id FROM tb_configurasi`
+    const update_reservation = `UPDATE tb_configurasi SET count_id = ?`
     const data = req.body
 
-    let date = data.detail_booking.room[0].date.split("/")
+    let date = data.detail_booking.date.split("-")
+    let date_time_slot = date.join("/")
     const a = date[0]
     date[0] = date[2]
     date[2] = date[1]
@@ -92,9 +94,11 @@ router.post('/booking', (req,res)=>{
 
     conn.query(count, (err, result_count) => {
         if (err) {
+            console.log(err);
             return res.send("error")
         } else {
-            result_count = result_count[0].count+1
+            console.log(result_count);
+            result_count = parseInt(result_count[0].count_id)+1
             if (result_count < 10) {
                 result_count = `000${result_count}`
             } else if (result_count < 100) {
@@ -103,10 +107,18 @@ router.post('/booking', (req,res)=>{
                 result_count = `0${result_count}`
             }
             let reservation_id = `${date}${result_count}`
-            conn.query(sql_reservation, [reservation_id,data.detail_booking.pic,detail_booking.jabatan,data.detail_booking.agenda,detail_booking.time,addon,user_booking],(err, result_reservation) => {
+            conn.query(sql_reservation, [reservation_id,data.detail_booking.pic,data.detail_booking.jabatan,data.detail_booking.agenda,data.detail_booking.time,addon,data.detail_booking.nama.length,user_booking],(err, result_reservation) => {
                 if (err) {
+                    console.log(err);
                     return res.send("error")
                 } else {
+                    conn.query(update_reservation, reservation_id, (err, result_update) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log(result_update);
+                        }
+                    })
                     for (let a = 0; a < data.detail_booking.nama.length; a++) {
                         let reminder = 0
                         let nik = 0
@@ -116,19 +128,22 @@ router.post('/booking', (req,res)=>{
                         }
                         conn.query(sql_participant, [nik,reservation_id,data.detail_booking.nama[a].nama,data.detail_booking.nama[a].hp,data.detail_booking.nama[a].email, reminder], (err, result_participant) => {
                             if (err) {
-                                console.log(err)
+                                console.log(err);
                             } else {
                                 console.log("success");
                             }
                         })
                     }
-                    conn.query(sql_time_slot, [data.detail_booking.room[0].ruangan_id,data.detail_booking.room[0].id,reservation_id,data.detail_booking.room[0].date], (err, result_participant) => {
-                        if (err) {
-                            console.log(err)
-                        } else {
-                            console.log("success");
-                        }
-                    })
+                    for (let i = 0; i < data.detail_booking.time_slot_id.length; i++) {
+                        conn.query(sql_time_slot, [data.detail_booking.ruangan.id,data.detail_booking.time_slot_id[i],reservation_id,date_time_slot], (err, result_participant) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                console.log("success");
+                            }
+                        })
+                        
+                    }
                     return res.send("Booking Berhasil Dibuat")
                 }
             })
